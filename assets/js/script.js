94e6edbdde8185eccce13b432b0cb32fcc508022
Amanda Klenk <amandamklenk3@gmail.com>
2022-05-05 10:40:18 -0400

    added to pseudocode, changed highScore to a number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 //Pseudocode for project
 //The site has the high score nav and the timer in the header, it has the title, description and start button upon visiting the site
-//When the user clicks the button
+//When the user clicks the button (event listener)
     //the timer starts
-    //the page changes to show the first quiz question
+    //the page changes to show the first quiz question (appendChild)
     //the high score nav is still on the page
-//When the user answers a question
+//When the user answers a question (event listner with data attributes)
     //each answer choice will
-        //display a prompt of correct or incorrect
-        //add to the high score or not (correct)
+        //display a prompt of correct or incorrect (pseudoelement)
+        //add to the high score or not (correct) (add to highScore)
         //take away from the timer or not (incorrect)
 //The user will repeat this with each question
 //the last question will direct the user to the high score page (as well as the nav button), bringing with it the high score
@@ -16,4 +16,4 @@
     //this array will be called upon with each browser refresh (parse)
     //the scores will be sorted in an ordered list (Array.sort)
 
-var highScore = [];
+var highScore = 0;
